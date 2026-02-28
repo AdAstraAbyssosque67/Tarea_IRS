c4e97e47f05768af2b2eff0de0a0bb3fc1ca4f58
AdAstraAbyssosque67 <jg2024-0823@uce.edu.do>
2026-02-27 23:35:23 -0400

    Add files via upload
    Código de la Primera Tarea

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace FlujodeControl_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su Sueldo:");
+            double Sueldo = Convert.ToDouble(Console.ReadLine());
+            double ISR = Sueldo switch
+            {
+                <= 34685 => 0,
+                <= 52027 => (Sueldo - 34685) * 0.15,
+                <= 72260 => 2601 + (Sueldo - 52027) * 0.20,
+                _ => 6648 + (Sueldo - 72260) * 0.25,
+            };
+        Console.Clear();    
+        Console.WriteLine($"Su sueldo es:  RD${Sueldo}");
+        Console.WriteLine();
+            if (ISR > 0)
+        Console.WriteLine($"Debe pagar de ISR: RD${ISR}");
+            else
+        Console.WriteLine("N/A");
+        }
+    }
+}
